31eab7dfb3181b9f595b7093a79e7525e7cca8a0
June Naruworn <naruwornv@gmail.com>
2021-05-08 11:10:07 +0700

    8-5-64

--- toooo-Front/src/config/axios.js
+++ toooo-Front/src/config/axios.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
+import localStorageService from '../services/localStorageService'
 
 axios.defaults.baseURL = "http://localhost:8000";
+
+axios.interceptors.request.use(
+    config => {
+        if(config.url.includes("/login") || config.url.includes("/register")) return comfig;
+
+        const token = localStorageService.getToken();
+
+        if(token){
+            config.headers["Autorization"] = `Bearer ${token}` ;
+    }
+        return config;
+    
+    },
+
+    err => {
+        Promise.reject(err);
+
+    }
+);
+
 
 export default axios;
